b8bc93b0702915a69985ae978cebf7914c910f97
Anxtt <elmagic11@abv.bg>
2024-03-01 05:10:16 +0200

    test 3

--- Server/TaskTracker.Api/Middleware/LoginRequiredMiddleware.cs
+++ Server/TaskTracker.Api/Middleware/LoginRequiredMiddleware.cs
@@ -1,32 +1,31 @@
 ﻿namespace TaskTracker.Api.Middleware
 {
-    public class LoginRequiredMiddleware
+    public class LoginRequiredMiddleware : IMiddleware
     {
-        private readonly RequestDelegate next;
+        //private readonly RequestDelegate next;
 
-        public LoginRequiredMiddleware(RequestDelegate next)
-            => this.next = next;
-    
-        public async Task Invoke(HttpContext context)
+        //public LoginRequiredMiddleware(RequestDelegate next)
+        //    => this.next = next;
+
+        //public async Task Invoke(HttpContext context)
+        //{
+            
+        //}
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            string verifiedCookie = context.Request.Cookies[".AspNetCore.Application.Verified"]!;
-            string cookie = context.Request.Cookies[".AspNetCore.Application.Cookie"]!;
-
-            if (verifiedCookie == null && cookie == null)
+            if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
             {
-                if (context.Request.Path != "/api/Identity/Login")
-                {
-                    //sort of works for now
-                    //gotta find a better way/improve this one to do this
-                    //implement log out and remove jwt cookie as a starter(?)
+                //sort of works for now
+                //gotta find a better way/improve this one to do this
+                //implement log out and remove jwt cookie as a starter(?)
 
-                    //return 405 Not Allowed
-                    //see how/what you can do with that
+                //return 405 Not Allowed
+                //see how/what you can do with that
 
-                    //redirect for log out needed
-                    context.Response.Redirect("/api/Identity/Login");
-                    return;
-                }
+                //redirect for log out needed
+                context.Response.Redirect("/api/Identity/Login");
+                return;
             }
 
             await next(context);
