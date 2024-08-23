a04280a740f51fb67a25349575470412a3aee3e3
Anxtt <elmagic11@abv.bg>
2024-08-23 15:32:42 +0300

    Added refresh token properties

--- Server/TaskTracker.Data/Migrations/20240816163209_RefreshTokenColumnInUser.Designer.cs
+++ Server/TaskTracker.Data/Migrations/20240816163209_RefreshTokenColumnInUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskTracker.Data;
 
@@ -11,9 +12,11 @@ using TaskTracker.Data;
 namespace TaskTracker.Data.Migrations
 {
     [DbContext(typeof(TaskTrackerDbContext))]
-    partial class TaskTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240816163209_RefreshTokenColumnInUser")]
+    partial class RefreshTokenColumnInUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -204,6 +207,7 @@ namespace TaskTracker.Data.Migrations
                         .HasColumnType("bit");
 
                     b.Property<string>("RefreshToken")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("SecurityStamp")
--- Server/TaskTracker.Data/Models/ApplicationUser.cs
+++ Server/TaskTracker.Data/Models/ApplicationUser.cs
@@ -5,5 +5,15 @@ namespace TaskTracker.Data.Models
     public class ApplicationUser : IdentityUser
     {
         public ICollection<Chore> Chores { get; } = new List<Chore>();
+
+        public string? RefreshToken { get; set; }
+
+        public DateTime Created { get; set; }
+        
+        public DateTime Expires { get; set; }
+
+        public bool IsExpired => DateTime.Now >= this.Expires;
+
+        public bool IsActive => this.IsExpired is false;
     }
 }
